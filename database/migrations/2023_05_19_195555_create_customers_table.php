680b14f854998a544291528ab7e6a9a93c9db2d5
Mario Aurelio Gasca López <49210976+mgasca93@users.noreply.github.com>
2023-05-19 14:11:32 -0600

    Se crea la conexion con la base de datos y se crea la estructura de la base

--- database/migrations/2023_05_19_195555_create_customers_table.php
+++ database/migrations/2023_05_19_195555_create_customers_table.php
@@ -11,13 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('customers', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('paternal_surname');
+            $table->string('email');
+            $table->unsignedBigInteger('administrator_id');
+            $table->foreign('administrator_id')->references('id')->on('administrators');
             $table->timestamps();
         });
     }
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('customers');
     }
 };
